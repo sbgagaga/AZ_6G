f892bc75d85eb6d3f6a41d2656c44d3892141c40
sbgagaga <464786640@qq.com>
2020-12-03 08:57:39 +0800

    正版

--- Keil5_AZ_6G/Objects/cf_log.d
+++
@@ -1,5 +0,0 @@
-.\objects\cf_log.o: user\cf_log.c
-.\objects\cf_log.o: D:\LoadDown\Kile\ARM\ARMCC\Bin\..\include\string.h
-.\objects\cf_log.o: user\cf_log.h
-.\objects\cf_log.o: D:\LoadDown\Kile\ARM\ARMCC\Bin\..\include\stdio.h
-.\objects\cf_log.o: D:\LoadDown\Kile\ARM\ARMCC\Bin\..\include\stdarg.h
--- Keil5_AZ_6G/Objects/startup_hc32f46x.d
+++
@@ -1,1 +0,0 @@
-.\objects\startup_hc32f46x.o: device\common\startup_hc32f46x.s
